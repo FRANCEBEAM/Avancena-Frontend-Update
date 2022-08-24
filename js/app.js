eb42e3eacff05bbe634c7370af3b80dd4a2d9496
Francis Beam Santander <santanderfrancisbeam@gmail.com>
2022-08-24 23:22:17 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// LETS CREATE SWIPER
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 15,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
